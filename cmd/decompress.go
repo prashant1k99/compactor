15c5052f7cefac208d6788ae437768d1c9f96e7a
Prashant <prashantco111@gmail.com>
2024-09-21 20:05:47 +0530

    Base setup for decompress logic

---
+++ cmd/decompress.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Step 1: Extract metadata from the inputFile
+// Step 2: Generate ReverseHuffmanCode using metadata
+// Step 3: Convert the bytes from the file to binaryString
+// Step 4: Using the ReverseHuffmanCode, convert the binaryString to correct bytes
+// Step 5: For last batch of the decoded, subtract the Padding Bits
+
+type ReverseHuffmanCode map[string]rune
+
+var (
+	reverseHuffmanCode   = make(ReverseHuffmanCode)
+	DecompressPercentage = 0
+)
+
+func ExtractMetadataFromFile(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	dataOffsetInt := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimRight(line, "\r\n")
+		dataOffsetInt += len(line) + 1
+
+		if line == "DATA_STARTS:" {
+			break
+		}
+		if strings.HasPrefix(line, "PaddingBits:") {
+			PaddingBits, _ = strconv.Atoi(strings.TrimPrefix(line, "PaddingBits:"))
+		} else {
+			parts := strings.SplitN(line, ":", 2)
+			if strings.Count(line, ":") == 2 {
+				parts = strings.SplitN(parts[1], ":", 2)
+				reverseHuffmanCode[parts[1]] = ':'
+				continue
+			}
+			if len(parts) == 2 {
+				var key rune
+				switch parts[0] {
+				case "":
+					key = '\n'
+				case "SPACE":
+					key = ' '
+				default:
+					key = []rune(parts[0])[0]
+				}
+				reverseHuffmanCode[parts[1]] = key
+			}
+		}
+	}
+
+	return dataOffsetInt
+}
+
+func ConvertBytesToBinary()
+
+func DecompressFile(inputFile, outputFilePath string) error {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println("Error while opening compressed file:")
+		return err
+	}
+	defer file.Close()
+
+	offsetBits := ExtractMetadataFromFile(file)
+
+	compressedFileStats, err := file.Stat()
+	if err != nil {
+		fmt.Println("Error while reading file stats")
+		return err
+	}
+	compressedFileSize := compressedFileStats.Size() - int64(offsetBits)
+	fmt.Println("FileSize", compressedFileSize)
+	fmt.Println("Will be outputed to:", outputFilePath)
+	return nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -57,12 +58,40 @@ var decompressCmd = &cobra.Command{
 }
 
 func decompressFile(cmd *cobra.Command, args []string) {
-	fmt.Println("Decompress command called")
+	inputFile, err := cmd.Flags().GetString("input")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	outputFilePath, err := cmd.Flags().GetString("output")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if outputFilePath == "" {
+		inputDir := filepath.Dir(inputFile)
+		outputFilePath = inputDir
+	}
+	inputFileName := filepath.Base(inputFile)
+	inputFileNameWithoutExt := strings.TrimSuffix(inputFileName, filepath.Ext(inputFileName))
+	outputFilePath = filepath.Join(outputFilePath, inputFileNameWithoutExt)
+
+	err = DecompressFile(inputFile, outputFilePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
 	rootCmd.Flags().StringP("input", "i", "", "Enter the path of the file to be compressed")
 	rootCmd.Flags().StringP("output", "o", "", "Enter the path for the output compressed file")
+	rootCmd.Flags().BoolP("help", "h", false, "Show help for all the options")
 	rootCmd.MarkFlagRequired("input")
+
+	decompressCmd.Flags().StringP("input", "i", "", "Enter file path of Compressed file")
+	decompressCmd.Flags().StringP("output", "o", "", "Enter path for decompressed file")
+	decompressCmd.MarkFlagRequired("input")
+	decompressCmd.Flags().BoolP("help", "h", false, "Show help for all the options")
 	rootCmd.AddCommand(decompressCmd)
 }
